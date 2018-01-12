f52ed54b45e80d45dcdab1972e4bf6d327cd2dd2
Sanghoo Park <sanghoo@nomadconnection.com>
2018-01-12 15:52:43 +0900

    axios 테스트 코드 호출

--- src/redux/api/rest.js
+++ src/redux/api/rest.js
@@ -1,22 +1,20 @@
 import axios from 'axios'
 
-const asxiosApi = axios.create({
+let asxiosApi = axios.create({
   baseURL: 'http://13.125.99.181:8080',
-  // withCredentials: true,
-  // headers: {
-  //   'Content-Type': 'application/json',
-  //   'Access-Control-Allow-Origin': '*'
-  // }
+  headers: {
+    'Content-Type': 'application/json',
+  }
 })
 
 export function postMainInfo() {
   return new Promise((resolve, reject) => {
-    axiosApi.post('/v0/main/mainInfo')
+    asxiosApi.post('/v0/main/mainInfo')
       .then(result => {
         resolve(result.data)
       })
       .catch(error => {
-        reject(error)    
+        reject(error)
       })
   })
 }
