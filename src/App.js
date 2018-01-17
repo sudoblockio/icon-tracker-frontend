cf999cde35920f83f791129823345cf29b8c1b8b
김인원 <inwonkim@icon.foundation>
2018-01-17 13:14:54 +0900

    App.js 복구..

--- src/App.js
+++ src/App.js
@@ -10,24 +10,9 @@ class App extends Component {
   render() {
     return (
       <Provider store={store}>
-        <div id="app-root">
-          <div className="root">
-            <div className={`wrap ${window.location.pathname === '/' ? 'home' : 'sub'}`}>
-              <Header />
-              <Route onEnter={window.scroll(0, 0)} exact path='/' component={MainPage}/>
-              <Route onEnter={window.scroll(0, 0)} exact path='/wallet/:pageId' component={AddressesPage}/>
-              <Route onEnter={window.scroll(0, 0)} path='/wallet/:addressId/:pageId' component={AddressDetailPage}/>
-              <Route onEnter={window.scroll(0, 0)} path='/transactions' component={TransactionsPage} exact/>
-              <Route onEnter={window.scroll(0, 0)} path='/transactions/:pageId' component={TransactionsPage}/>
-              <Route onEnter={window.scroll(0, 0)} path='/blocks' component={BlocksPage} exact />
-              <Route onEnter={window.scroll(0, 0)} path='/blocks/:pageId' component={BlocksPage}/>
-              <Route onEnter={window.scroll(0, 0)} path='/block/:id' component={BlockDetailPage} exact />
-              <Route onEnter={window.scroll(0, 0)} path='/block/:id/:pageId' component={BlockDetailPage}/>
-            </div>
-            <div className="blank"></div>
-          </div>
-          <Footer />
-        </div>
+        <ConnectedRouter history={history}>
+          <Routes />
+        </ConnectedRouter>
       </Provider>
     );
   }
