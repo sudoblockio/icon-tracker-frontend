fb7ec767d82d6c4752ed2aa3fe8d2de90dbc5007
Sanghoo Park <sanghoo@nomadconnection.com>
2018-07-16 10:30:09 +0900

    작업 중

---
+++ src/components/TokensPage/TokenDetailPage.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { withRouter } from 'react-router-dom';
+
+class TokenDetailPage extends Component {
+    render() {
+        return (
+            <div></div>
+        )
+    }
+}
+
+export default withRouter(TokenDetailPage);
