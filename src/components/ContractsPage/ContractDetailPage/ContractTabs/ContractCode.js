8bd91ccb54d47ac10c4305191c277715f33645a9
Sanghoo Park <sanghoo@nomadconnection.com>
2018-08-30 18:59:37 +0900

    Remove console.log()

--- src/components/ContractsPage/ContractDetailPage/ContractTabs/ContractCode.js
+++ src/components/ContractsPage/ContractDetailPage/ContractTabs/ContractCode.js
@@ -30,7 +30,6 @@ class ContractCode extends Component {
         if (!!address) {
             const activeLink = isValidData(contractVersion) ? await makeDownloadLink(address, contractVersion) : ''
             const updatedLink = isValidData(newVersion) ? await makeDownloadLink(address, newVersion) : ''
-            console.log(contractVersion, newVersion, activeLink, updatedLink)
             this.setState({ activeLink, updatedLink })
         }
     }
@@ -41,7 +40,6 @@ class ContractCode extends Component {
         const { data } = contract
         const { address, tokenName, symbol, contractVersion, newVersion } = data
         const { loading, data: abiData, error } = contractAbi
-        console.log(activeLink)
         return (
             <div className="contents">
                 <table className="table-typeL">
