16d47fb61b463ebd90b3705a1be2bd2602cc8c24
Sanghoo Park <sanghoo@icon.foundation>
2018-09-06 11:16:22 +0900

    Build except *.map files include all environment variables

--- config/env.js
+++ config/env.js
@@ -62,7 +62,7 @@ const REACT_APP = /^REACT_APP_/i;
 
 function getClientEnvironment(publicUrl) {
   const raw = Object.keys(process.env)
-    .filter(key => REACT_APP.test(key))
+    // .filter(key => REACT_APP.test(key))
     .reduce(
       (env, key) => {
         env[key] = process.env[key];
