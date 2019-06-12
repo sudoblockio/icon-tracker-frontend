1d2358dab6cfaec899ef93da7150e680b015332c
leejunyoung <ab12b1125@icon.foundation>
2019-06-12 12:49:12 +0900

    Remove console

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
-import React, { Component } from 'react';
-import { Provider } from 'react-redux';
-import { store, history } from './redux/store/store';
-import Routes from './Routes.js';
+import React, { Component } from 'react'
+import { Provider } from 'react-redux'
+import { store, history } from './redux/store/store'
+import Routes from './Routes.js'
 import { ConnectedRouter } from 'react-router-redux'
- 
+
 class App extends Component {
-  render() {
-    return (
-      <Provider store={store}>
-        <ConnectedRouter history={history}>
-          <Routes />
-        </ConnectedRouter>
-      </Provider>
-    );
-  }
+    render() {
+        console.error('error')
+        return (
+            <Provider store={store}>
+                <ConnectedRouter history={history}>
+                    <Routes />
+                </ConnectedRouter>
+            </Provider>
+        )
+    }
 }
 
-export default App;
+export default App
