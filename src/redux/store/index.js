9e4cdd20966186fe673c811ed88cf72c7a28f169
Rheannone <73176069+Rheannone@users.noreply.github.com>
2021-08-11 10:24:53 -0400

    basic config object concept

--- src/redux/store/index.js
+++ src/redux/store/index.js
@@ -16,6 +16,7 @@ const createRootReducer = (history) => combineReducers ({
 });
 
 let enhancer;
+
 // TODO: add all the env vars
 if (process.env.NODE_ENV === "production") {
     enhancer = applyMiddleware(thunk);
@@ -25,6 +26,30 @@ if (process.env.NODE_ENV === "production") {
       window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
     enhancer = composeEnhancers(applyMiddleware(thunk, logger));
   }
+
+  // if (process.env.REACT_APP_ENV) {
+  //   switch (process.env.REACT_APP_ENV) {
+  //     case 'mainnet':
+  //       return 'https://tracker.icon.foundation';
+  //     // return 'https://bicon.tracker.solidwallet.io';
+  //     case 'testnet':
+  //       return 'https://trackerdev.icon.foundation';
+  //     // return 'http://10.201.11.74:8081';
+  //     case 'testnet1':
+  //       return 'https://bicon.tracker.solidwallet.io';
+  //     // return 'http://10.201.11.74:8081';
+  //     case 'sejong':
+  //       return 'https://sejong.tracker.solidwallet.io';
+  //     case 'custom':
+  //       return 'http://trackerlocaldev.icon.foundation';
+  //     case 'prep':
+  //     case 'np':
+  //       return 'http://54.180.16.76';
+  //     case 'qa':
+  //       return 'http://13.125.236.68';
+  //     default:
+  //   }
+  // }
 
 export const history = createBrowserHistory()
 const configureStore = (preloadedState) => {
