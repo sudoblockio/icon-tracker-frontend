479c0783961c05550194f6f576397ecd0adfbd67
Rheannone <73176069+Rheannone@users.noreply.github.com>
2021-08-17 10:38:52 -0400

    starting search tests

--- config/default.js
+++ config/default.js
@@ -1,35 +1,47 @@
-const env = process.env.NODE_ENV;
-// !Important! All variables on this file are written directly to the frontend. Sensitive 
-// data must be abstracted through dotenv.
+// const env = process.env.NODE_ENV;
+// // !Important! All variables on this file are written directly to the frontend. Sensitive 
+// // data must be abstracted through dotenv.
 
-const dev = {
-    app = {
-        // all process.env variables are Strings, 
-        // if a number is needed, use parseInt
-        port: parseInt(process.env.DEV_APP_PORT) || 3000
-    },
-    db = {
-        // test values
-        host: process.env.DEV_DB_HOST || 'localhost',
-        port: parseInt(process.env.DEV_DB_PORT) || 3000,
-        name: process.env.DEV_DB_NAME || 'db'
-    }
-}
+// const dev = {
+//     app = {
+//         // all process.env variables are Strings, 
+//         // if a number is needed, use parseInt
+//         port: parseInt(process.env.DEV_APP_PORT) || 3000
+//     },
+//     db = {
+//         // test values
+//         host: process.env.DEV_DB_HOST || 'localhost',
+//         port: parseInt(process.env.DEV_DB_PORT) || 3000,
+//         name: process.env.DEV_DB_NAME || 'db'
+//     }
+// }
 
-const test = {
-    app = {
-        port: 3000
-    },
-    db = {
-        // test values
-        host: localhost,
-        port: 3000
-    }
-}
+// const test = {
+//     app = {
+//         port: 3000
+//     },
+//     db = {
+//         // test values
+//         host: localhost,
+//         port: 3000
+//     }
+// }
 
-const config = {
-    dev,
-    test
-}
+// const mainnet = {
+//     app = {
+//         port: 3000
+//     },
+//     db = {
+//         // test values
+//         host: localhost,
+//         port: 3000
+//     }
+// }
 
-module.exports = config[env]
+// const config = {
+//     dev,
+//     test,
+//     mainnet
+// }
+
+// module.exports = config[env]
--- src/components/CommonComponent/SearchInput.test.js
+++ src/components/CommonComponent/SearchInput.test.js
@@ -1,9 +1,15 @@
 // import handleClick from '../SearchInput'
 // import select needed "dispatches" from store to run test
 // STUB:
-const blockTests = []
+const blockTests = [38426517,38426540]
 const addressTests = []
 
 // check what type of query, for any structure of query 
 // check that it returned correct dispatch for type
-// 
+
+it("correctly verifies block regex", () => {
+   const block_Regex = new RegExp('([0-9][1-9][0-9]{1,7}|100000000)')
+   blockTests.forEach(block => {
+       expect(block.toString()).toMatch(block_Regex)
+   })
+})
