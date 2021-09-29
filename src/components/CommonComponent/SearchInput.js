e2354311e71a1c16821d19307603281257d9c083
Rheannone <73176069+Rheannone@users.noreply.github.com>
2021-09-29 13:34:46 -0400

    select random test data function

--- src/components/CommonComponent/SearchInput.js
+++ src/components/CommonComponent/SearchInput.js
@@ -1,6 +1,7 @@
 import React, {useState} from 'react';
 import { useDispatch } from 'react-redux';
 import searchBlocks from '../../redux/store/search'
+
 export const block_re = new RegExp('([0-9][1-9][0-9]{1,7}|100000000)')
 export const add_re = new RegExp('^hx[a-fA-F0-9]{40}$')
 export const tx_re = new RegExp('^0x([A-Fa-f0-9]{64})$')
@@ -33,10 +34,7 @@ function SearchInput() {
   const handleClick = (e) => {
     const searchTerm = e.target.value
     let searchByType = (searchTerm) => {
-      return block_re.test(searchTerm) === true ? dispatch(searchBlocks(searchTerm))
-            : add_re.test(searchTerm) === true ? dispatch(/*searchAdd(searchTerm)*/)
-            : tx_re.test(searchTerm) === true ? dispatch(/*searchTx(searchTerm)*/)
-            : setError("Not found")
+      return block_re.test(searchTerm) === true ? dispatch(searchBlocks(searchTerm)) : setError("Not found")
     } 
 
     searchByType(searchTerm)
