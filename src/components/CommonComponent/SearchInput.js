780d46dc3159808bbaf2c20e58338223b96c1540
Rheannone <73176069+Rheannone@users.noreply.github.com>
2021-10-06 11:13:48 -0400

    trying search as a class component

--- src/components/CommonComponent/SearchInput.js
+++ src/components/CommonComponent/SearchInput.js
@@ -1,68 +1,74 @@
-import React, {useState} from 'react';
-import { useDispatch } from 'react-redux';
-import { Redirect } from 'react-router';
-import {searchBlocks} from '../../redux/store/search'
+import React, { Component } from 'react';
+import { searchBlocks } from '../../redux/store/search'
 
-
-export const block_re = new RegExp('([0-9][1-9][0-9]{1,7}|100000000)')
-export const add_re = new RegExp('^hx[a-fA-F0-9]{40}$')
-export const tx_re = new RegExp('^0x([A-Fa-f0-9]{64})$')
-let searchByType; 
-
-function SearchInput() {
-  const dispatch = useDispatch();
-  const [ search, setSearch] = useState("")
-  const [ errors, setError ] = useState("")
-
-
-  const handleChange = (e) => {
-    setSearch(e.target.value)
+class SearchInput extends Component {
+  constructor(props) {
+    super(props)
+    const { searchKeyword } = this.props
+    this.state = {
+      search: searchKeyword
+    }
   }
 
-  const handleKeyDown = (e) => {
-    if (e.key === 'Enter') {
-      handleClick(e)
+  componentDidMount = () => {
+    const { searchKeyword, id } = this.props
+    if (searchKeyword && id) {
+      document.getElementById(id).focus()
     }
-    // keycode 27 = `esc`
+  }
+
+  handleChange = (e) => {
+    const { name, value } = e.target
+    const prevSearch = this.state[name]
+    this.setState({ [name]: value }, () => {
+      if (prevSearch !== '' && value === '') {
+        this.handleClick()
+      }
+    })
+  }
+
+  handleKeyDown = (e) => {
+    if (e.key === 'Enter') {
+      this.handleClick()
+    }
     if (e.keyCode === 27) {
       const { name } = e.target
       this.setState({ [name]: '' }, () => {
-        handleClick()
+        this.handleClick()
       })
     }
   }
 
-  const handleClick = (e) => {
-    const searchTerm = e.target.value
-    console.log(searchTerm, "search term")
-    
-    if (block_re.test(searchTerm)) {
-      console.log("it is a block")
-      dispatch(searchBlocks(searchTerm))
-      this.props.history.push(`/blocks/${searchTerm}`)
-    } else {
-      setError("Not Found")
-    }
-
+  handleFocus = (e) => {
+    const { value } = e.target
+    e.target.value = ''
+    e.target.value = value
   }
 
+  handleClick = (dispatch) => {
+    const { search } = this.state
+    dispatch(searchBlocks(search))
+  }
+
+  render() {
+    const { search } = this.state
+    const { placeholder, id } = this.props
     return (
       <div className="search-holder">
         <div className="search-group">
           <input name="search" type="text" className="txt-type-search"
-            placeholder="Address, TxHash, Block, SCORE"
+            id={id}
+            placeholder={placeholder}
             value={search}
-            onChange={handleChange}
-            onKeyDown={handleKeyDown}
+            onChange={this.handleChange}
+            onKeyDown={this.handleKeyDown}
+            onFocus={this.handleFocus}
           />
-          <div className="error-holder">
-            <div><p id="error-message">{errors}</p></div>
-          </div>
-          <span onClick={handleClick}><em className="img"></em></span>
+          <span onClick={this.handleClick}><em className="img"></em></span>
         </div>
       </div>
     )
-  
+  }
 }
 
-export  {SearchInput, searchByType}
+export {SearchInput}
