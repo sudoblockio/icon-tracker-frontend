3aa78012ff33f6f2b241fa214546411a9876207a
rheannone <rheannone@gmail.com>
2021-11-18 16:51:58 -0500

    dot io not dot net

--- src/config.js
+++ src/config.js
@@ -1,20 +1,20 @@
 let backendEndpoint;
 // *** ready for env var:
-// switch (process.env.DEPLOYMENT_ENVIRONMENT) {
-//     case 'development':
-//         backendEndpoint = 'https://explorer.icon.geometry-dev.net'
-//         break
-//     case 'production' :
-//         backendEndpoint = 'https://explorer.icon.geometry.net'
-//         break
-// }
-
+switch (process.env.DEPLOYMENT_ENVIRONMENT) {
+    case 'development':
+        backendEndpoint = 'https://explorer.icon.geometry-dev.net'
+        break
+    case 'production' :
+        backendEndpoint = 'https://explorer.icon.geometry.io'
+        break
+}
+console.log(process.env, "the whole env")
 
 export const configJson =
  {   
     "VERSION": "1.0.18_20190313_0",
     "To use below variables,": "remove two underbars at the front, and then set as you want.",
-    "TRACKER_API_URL": 'https://explorer.icon.geometry-dev.net',
+    "TRACKER_API_URL": `${backendEndpoint}`,
     "WALLET_API_URL": "https://wallet.icon.foundation",
     "__IS_SOLO_VERSION": false
 }
