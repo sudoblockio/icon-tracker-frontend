8e6c0d1799ef3dc8e411924112c53d34fe9a5261
Richard Mah <dick.mah@gmail.com>
2021-11-18 15:34:20 -0800

    whoops

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 let configJson;
-switch (process.env.REACT_APP_DEPLOYMENT_ENVIRONMENT || process.env.NODE_ENV) {
-    case 'dev' || 'development':
+switch (process.env.REACT_APP_DEPLOYMENT_ENVIRONMENT) {
+    case 'dev':
         configJson =  {   
             "VERSION": "1.0.18_20190313_0",
             "To use below variables,": "remove two underbars at the front, and then set as you want.",
