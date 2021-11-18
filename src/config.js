a5a2d4fd2fcfc2abc20ab8eb755fcb8d3fa1d9d0
rheannone <rheannone@gmail.com>
2021-11-18 15:41:26 -0500

    comment out new env setup

--- src/config.js
+++ src/config.js
@@ -1,17 +1,20 @@
 let backendEndpoint;
-switch (process.env.DEPLOYMENT_ENVIRONMENT || process.env.NODE_ENV) {
-    case 'development':
-        backendEndpoint = 'https://explorer.icon.geometry-dev.net'
-        break
-    case 'production' :
-        backendEndpoint = 'https://explorer.icon.geometry.net'
-        break
-}
+// *** remove or case when issue is closed. Only needed temporarily.  
+// switch (process.env.DEPLOYMENT_ENVIRONMENT) {
+//     case 'development':
+//         backendEndpoint = 'https://explorer.icon.geometry-dev.net'
+//         break
+//     case 'production' :
+//         backendEndpoint = 'https://explorer.icon.geometry.net'
+//         break
+// }
+
+
 export const configJson =
  {   
     "VERSION": "1.0.18_20190313_0",
     "To use below variables,": "remove two underbars at the front, and then set as you want.",
-    "TRACKER_API_URL": `${backendEndpoint}`,
+    "TRACKER_API_URL": 'https://explorer.icon.geometry-dev.net',
     "WALLET_API_URL": "https://wallet.icon.foundation",
     "__IS_SOLO_VERSION": false
 }
