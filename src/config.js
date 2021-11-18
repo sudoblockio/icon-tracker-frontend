26adf8c61b8f9cc9183d260524aa6936b86d9aea
rheannone <rheannone@gmail.com>
2021-11-18 18:39:47 -0500

    mixed content

--- src/config.js
+++ src/config.js
@@ -4,7 +4,7 @@ switch (process.env.REACT_APP_DEPLOYMENT_ENVIRONMENT) {
         configJson =  {   
             "VERSION": "1.0.18_20190313_0",
             "To use below variables,": "remove two underbars at the front, and then set as you want.",
-            "TRACKER_API_URL": 'http://explorer.icon.geometry-dev.net',
+            "TRACKER_API_URL": 'https://explorer.icon.geometry-dev.net',
             "WALLET_API_URL": "https://wallet.icon.foundation",
             "__IS_SOLO_VERSION": false}
         break
@@ -12,7 +12,7 @@ switch (process.env.REACT_APP_DEPLOYMENT_ENVIRONMENT) {
         configJson =  {   
             "VERSION": "1.0.18_20190313_0",
             "To use below variables,": "remove two underbars at the front, and then set as you want.",
-            "TRACKER_API_URL": 'http://explorer.icon.geometry.io',
+            "TRACKER_API_URL": 'https://explorer.icon.geometry.io',
             "WALLET_API_URL": "https://wallet.icon.foundation",
             "__IS_SOLO_VERSION": false}
         break
