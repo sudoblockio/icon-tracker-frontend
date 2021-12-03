dda01b13b0a8d40906f77ac3fbf9edbd1e053962
rheannone <rheannone@gmail.com>
2021-12-03 10:13:57 -0500

    there is a form

--- src/components/Popup/AddressQrCode.js
+++ src/components/Popup/AddressQrCode.js
@@ -23,38 +23,59 @@ class AddressQrCode extends Component {
                     </section>
                     <div>
                     <section>
-                        <h4>Addt'l Details</h4>
-                        <p>
-                        Name: <input class="txt-type-search modified" type="text" name="name" />
+                        <h6>Addt'l Details</h6>
+
+                        <div className="cv-label-container">
+                        <p className="cv-label">
+                        Name:</p>
+                         <input className="txt-type-search modified cv" type="text" name="name" />
+                         </div>
+
+                        <div className="cv-label-container">
+                        <p className="cv-label">
+                        Website: 
                         </p>
-                        <p>
-                        Website: <input class="txt-type-search modified" type="text" name="website" />
+                        <input class="txt-type-search modified" type="text" name="website" />
+                        </div>
+
+                        <div className="cv-label-container">
+                        <p className="cv-label">
+                        Email: 
                         </p>
-                        <p>
-                        Email: <input class="txt-type-search modified" type="email" name="email" />
+                        <input class="txt-type-search modified" type="email" name="email" />
+                        </div>
+
+
+                        <div className="cv-label-container">
+                        <p className="cv-label">
+                        p_rep Address:
                         </p>
-                        <p>
-                        p_rep Address: <input class="txt-type-search modified" type="text" name="prep_address" />
-                        </p>
+                        <input class="txt-type-search modified" type="text" name="prep_address" />
+                        </div>
                     </section>
                     </div>
                     <br/>
                     <div>
                         <section>
-                            <h4>Social Accounts</h4>
-                            <p>
-                            Github: <input class="txt-type-search modified" type="email" name="email" />
-                            </p>
-                            <p>
-                            Twitter: <input class="txt-type-search modified" type="email" name="email" />
-                            </p>
-                            <p>
-                            Telegram: <input class="txt-type-search modified" type="email" name="email" />
-                            </p>
+                            <h6>Social Accounts</h6>
+                            <div className="cv-label-container">
+                            <p className="cv-label">
+                            Github: </p><input class="txt-type-search modified" type="email" name="email" />
+                            </div>
+                            <div className="cv-label-container">
+                            <p className="cv-label">
+                            Twitter:</p> <input class="txt-type-search modified" type="email" name="email" />
+                            </div>
+                            <div className="cv-label-container">
+                            <p className="cv-label">
+                            Telegram:</p><input class="txt-type-search modified" type="email" name="email" />
+                            </div>
                         </section>
                     </div>
                 </form>
+
                 <div className='btn-holder full'>
+                <input type="file" id="myFile" name="filename"/>
                     <button className='btn-type-normal size-full'>
                         <span>Submit</span>
                     </button>
