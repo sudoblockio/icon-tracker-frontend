7637c97dcc1b6a32cf2b26176c7e1ae78650149c
Richard Mah <dick.mah@gmail.com>
2021-12-17 14:51:36 -0800

    update workflows for new testnets

--- src/config.js
+++ src/config.js
@@ -14,18 +14,25 @@ switch (`${process.env.REACT_APP_DEPLOYMENT_ENVIRONMENT}` + `-` + `${process.env
             "WALLET_API_URL": "https://api.icon.geometry.io",
             "__IS_SOLO_VERSION": false}
         break
-    case 'dev-sejong' :
-        configJson =  {   
-            "VERSION": "1.0.18_20190313_0",
-            "TRACKER_API_URL": 'https://explorer.sejong.geometry-dev.net',
-            "WALLET_API_URL": "https://icon.geometry-dev.net",
-            "__IS_SOLO_VERSION": false}
-        break
     case 'prod-sejong' :
         configJson =  {   
             "VERSION": "1.0.18_20190313_0",
             "TRACKER_API_URL": 'https://explorer.sejong.geometry.io',
             "WALLET_API_URL": "https://wallet.icon.foundation",
+            "__IS_SOLO_VERSION": false}
+        break
+    case 'prod-berlin' :
+        configJson =  {
+            "VERSION": "1.0.18_20190313_0",
+            "TRACKER_API_URL": 'https://tracker.berlin.geometry.io',
+            "WALLET_API_URL": "https://berlin.net.solidwallet.io/",
+            "__IS_SOLO_VERSION": false}
+        break
+    case 'prod-lisbon' :
+        configJson =  {
+            "VERSION": "1.0.18_20190313_0",
+            "TRACKER_API_URL": 'https://tracker.lisbon.geometry.io',
+            "WALLET_API_URL": "https://lisbon.net.solidwallet.io/",
             "__IS_SOLO_VERSION": false}
         break
 }
