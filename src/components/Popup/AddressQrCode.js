00ef06abbce7c3ff35435d2129e4c2d99c8e207f
rheannone <rheannone@gmail.com>
2022-02-02 10:25:34 -0500

    improved form UI

--- src/components/Popup/AddressQrCode.js
+++ src/components/Popup/AddressQrCode.js
@@ -29,6 +29,7 @@ class AddressQrCode extends Component {
             youtube: "",
             zipped_source_code: "",
             isActive: false,
+            flipLicense: false,
         }
     }
     // accepts our user input file object[0].
@@ -167,9 +168,14 @@ class AddressQrCode extends Component {
         this.setState({zipped_source_code: file})
     }
     labels;
-
+    accordion='acc-closed'
     flipActive=() => {
         this.setState({isActive: !this.state.isActive})
+        this.accordion=!this.accordion
+    }
+
+    flipLicense = () => {
+        this.setState({flipLicense: !this.state.flipLicense})
     }
     componentDidMount() {
         // this.labels = document.getElementsByClassName("legend")
@@ -189,12 +195,12 @@ class AddressQrCode extends Component {
                 <h1 key='h1' className="title">Submit a Contract for Verification</h1>
                 <div className="cv-form-container verify">
                     <form actionmethod="POST"onSubmit={(e) => this.handleSubmit(e)} encType="multipart/form-data" id="contractform">
-                        <div className="cv-label-container">
+                        <div className="cv-label-container verify-row">
                             <p className="cv-label">
                                 Wallet: </p><input className="txt-type-search modified" type="text" name="address" readOnly={true} value={address} placeholder={address} />
 
                         </div>
-                        <div className="cv-label-container">
+                        <div className="cv-label-container verify-row">
                             <p className="cv-label">
                                 Contract: </p><input className="txt-type-search modified" type="text" name="contract" readOnly={true} value={this.props.data.contract} placeholder={this.props.data.contract} />
 
@@ -210,40 +216,36 @@ class AddressQrCode extends Component {
                             <div>
                                 <section>
 
-                                    <div className="cv-label-container">
+                                    <div className="cv-label-container verify-row">
                                         <p className="cv-label">
                                             Team Name:</p>
-                                        <input className="txt-type-search modified cv" autocomplete="organization" value={this.state.name} type="text" name="name" onChange={(e) => this.setName(e)} />
-                                    </div>
-                                    <div className="cv-label-container">
-                                        <p className="cv-label">
-                                            Short Desc:</p>
-                                        <input className="txt-type-search modified cv" autocomplete="off" value={this.state.shortDesc} type="text" name="shortdesc" onChange={(e) => this.setShortDesc(e)} />
-                                    </div>
-                                    <div className="cv-label-container">
-                                        <p className="cv-label">
-                                            Long Desc:</p>
-                                        <textarea rows="3" cols="41" className="modified cv" autocomplete="off" value={this.state.longDesc} type="textarea" name="longdesc" onChange={(e) => this.setLongDesc(e)} />
+                                        <input className="verify-row txt-type-search" autocomplete="organization" value={this.state.name} type="text" name="name" onChange={(e) => this.setName(e)} />
                                     </div>
 
-                                    <div className="cv-label-container">
+
+
+                                    <div className="cv-label-container verify-row">
                                         <p className="cv-label">
                                             City:
                                         </p>
                                         <input className="txt-type-search modified" type="text" name="city" autocomplete="city" value={this.state.city} onChange={(e) => this.setCity(e)} />
                                     </div>
 
-                                    <div className="cv-label-container">
+                                    <div className="cv-label-container verify-row">
                                         <p className="cv-label">
                                             Country:
                                         </p>
                                         <input class="txt-type-search modified" type="country" name="country" autocomplete="country-name" value={this.state.country} onChange={(e) => this.setCountry(e)} />
                                     </div>
-                                    <div className="cv-label-container">
+                                    <div className="cv-label-container verify-row">
                                         <p className="cv-label">
                                             Website:
                                         </p>
                                         <input class="txt-type-search modified" type="website" name="website" autocomplete="website" value={this.state.website} onChange={(e) => this.setWebsite(e)} />
+                                    </div>
+                                    <div className="cv-label-container verify-row">
+                                        <p className="cv-label">
+                                            .zip File:</p><input type="file" accept=".zip" id="contractzip" name="cxsrccode" onChange={(e) => this.setZip(e.target.value)}/>
                                     </div>
 
 
@@ -254,63 +256,75 @@ class AddressQrCode extends Component {
                         <input class="txt-type-search modified" type="text" name="prep_address" />
                         </div> */}
                                 </section>
-                                <div className = 'accordion-top' onClick={() => this.flipActive()}>
-                                    <div>{this.state.isActive ? '-' : '+'}</div>
+                                <div className = 'accordion-top' >
+                                    <div className="cv-label verify-title" onClick={() => this.flipActive()}>{this.state.isActive ? '▼ More Details' : '► More Details:'}</div>
                                     {this.state.isActive && 
-                                    <>
-                                    <div className="cv-label-container">
+        
+                                    <div className={`${this.accordion}`}>
+                                                                                  <div className="cv-label-container">
+                                        <p className="cv-label">
+                                            Short Desc:</p>
+                                        <input className="txt-type-search modified cv" autocomplete="off" value={this.state.shortDesc} type="text" name="shortdesc" onChange={(e) => this.setShortDesc(e)} />
+                                    </div>                              <div className="cv-label-container">
+                                        <p className="cv-label">
+                                            Long Desc:</p>
+                                        <textarea rows="3" cols="41" className="modified cv" autocomplete="off" value={this.state.longDesc} type="textarea" name="longdesc" onChange={(e) => this.setLongDesc(e)} />
+                                    </div>
+                                    <div className="cv-label-container verify-row">
                                         <p className="cv-label">
                                             Github: </p><input class="txt-type-search modified" type="text" name="github" value={this.state.github} onChange={(e) => this.setGithub(e)} />
                                     </div>
-                                    <div className="cv-label-container">
+                                    <div className="cv-label-container verify-row">
                                         <p className="cv-label">
                                             Twitter:</p> <input class="txt-type-search modified" type="text" name="twitter" placeholder={"@handle"} value={this.state.twitter} onChange={(e) => this.setTwitter(e)} />
                                     </div>
-                                    <div className="cv-label-container">
+                                    <div className="cv-label-container verify-row">
                                         <p className="cv-label">
                                             Telegram:</p><input class="txt-type-search modified" type="text" name="telegram" value={this.state.telegram} onChange={(e) => this.setTelegram(e)} />
                                     </div>
-                                    <div className="cv-label-container">
+                                    <div className="cv-label-container verify-row">
                                         <p className="cv-label">
                                             Reddit: </p><input class="txt-type-search modified" type="text" name="reddit" value={this.state.reddit} onChange={(e) => this.setReddit(e)} />
                                     </div>
-                                    <div className="cv-label-container">
+                                    <div className="cv-label-container verify-row">
                                         <p className="cv-label">
                                             Youtube:</p> <input class="txt-type-search modified" type="text" name="youtube" value={this.state.youtube} onChange={(e) => this.setYoutube(e)} />
                                     </div>
-                                    <div className="cv-label-container">
+                                    <div className="cv-label-container verify-row">
                                         <p className="cv-label">
                                             Facebook:</p><input class="txt-type-search modified" type="text" name="facebook" value={this.state.facebook} onChange={(e) => this.setFacebook(e)} />
                                     </div>
-                                    <div className="cv-label-container">
+                                    <div className="cv-label-container verify-row">
                                         <p className="cv-label">
                                             Discord:</p><input class="txt-type-search modified" type="text" name="discord" value={this.state.discord} onChange={(e) => this.setDiscord(e)} />
                                     </div>
-                                    <div className="cv-label-container">
+                                    <div className="cv-label-container verify-row">
                                         <p className="cv-label">
                                             Steemit:</p><input class="txt-type-search modified" type="text" name="steemit" value={this.state.steemit} onChange={(e) => this.setSteemit(e)} />
                                     </div>
-                                    <div className="cv-label-container">
+                                    <div className="cv-label-container verify-row">
                                         <p className="cv-label">
                                             WeChat:</p><input class="txt-type-search modified" type="text" name="wechat" value={this.state.wechat} onChange={(e) => this.setWeChat(e)} />
                                     </div>
-                                    <div className="cv-label-container">
+                                    <div className="cv-label-container verify-row">
                                         <p className="cv-label">
                                             Keybase:</p><input class="txt-type-search modified" type="text" name="keybase" value={this.state.keybase} onChange={(e) => this.setKeybase(e)} />
                                     </div>
-                                    </>
+                                    </div>
                                     
                                     
+                                    
+                                      }  
+
+                                    <div className="cv-label verify-title" onClick={() => this.flipLicense()}>{this.state.flipLicense ? '▼ License' : '► License:'}</div>
+                                    {this.state.flipLicense &&
+                                    <div className="cv-label-container verify-row">
+                                        <p className="cv-label">
+                                            License:</p><input class="txt-type-search modified" type="text" name="keybase" placeholder="dropdown" value={this.state.keybase} onChange={(e) => this.setKeybase(e)} />
+                                    </div>
                                     
                                     }
-                                    <div className="cv-label-container">
-                                        <p className="cv-label">
-                                            License:</p><input class="txt-type-search modified" type="text" name="keybase" value={this.state.keybase} onChange={(e) => this.setKeybase(e)} />
-                                    </div>
-                                    <div className="cv-label-container">
-                                        <p className="cv-label">
-                                            .zip File:</p><input type="file" accept=".zip" id="contractzip" name="cxsrccode" onChange={(e) => this.setZip(e.target.value)}/>
-                                    </div>
+
 
 
                                 </div>
