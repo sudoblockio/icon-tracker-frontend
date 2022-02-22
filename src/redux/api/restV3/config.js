324b49fd70aa8b1d88add8fc8194d6aa4dd1d24f
rheannone <rheannone@gmail.com>
2022-02-22 15:35:55 -0500

    trackerApiConfig simplified

--- src/redux/api/restV3/config.js
+++ src/redux/api/restV3/config.js
@@ -16,112 +16,19 @@ export async function walletApiInstance() {
 }
 
 export async function getTrackerApiUrl() {
-  const configFile = await getConfigJsonFile();
-  if (configFile && configFile.TRACKER_API_URL) {
-    return configFile.TRACKER_API_URL;
+  if (configJson && configJson.TRACKER_API_URL) {
+    return configJson.TRACKER_API_URL;
   }
-
-  // if (process.env.REACT_APP_ENV) {
-  //   switch (process.env.REACT_APP_ENV) {
-  //     case 'mainnet':
-  //       return 'https://tracker.icon.foundation';
-  //     // return 'https://bicon.tracker.solidwallet.io';
-  //     case 'testnet':
-  //       return 'https://trackerdev.icon.foundation';
-  //     // return 'http://10.201.11.74:8081';
-  //     case 'testnet1':
-  //       return 'https://bicon.tracker.solidwallet.io';
-  //     // return 'http://10.201.11.74:8081';
-  //     case 'sejong':
-  //       return 'https://sejong.tracker.solidwallet.io';
-  //     case 'custom':
-  //       return 'http://trackerlocaldev.icon.foundation';
-  //     case 'prep':
-  //     case 'np':
-  //       return 'http://54.180.16.76';
-  //     case 'qa':
-  //       return 'http://13.125.236.68';
-  //     default:
-  //   }
-  // }
-
-  // return '/';
 }
 
 export async function getWalletApiUrl() {
-  const configFile = await getConfigJsonFile();
-  if (configFile && configFile.WALLET_API_URL) {
-    return configFile.WALLET_API_URL;
-  }
-
-  // if (process.env.REACT_APP_ENV) {
-  //   switch (process.env.REACT_APP_ENV) {
-  //     case 'mainnet':
-  //       return 'https://wallet.icon.foundation';
-  //     case 'testnet':
-  //       return 'https://testwallet.icon.foundation';
-  //     // return 'http://13.125.65.157:9000';
-  //     case 'testnet1':
-  //       return 'https://bicon.net.solidwallet.io';
-  //     // return 'http://13.125.65.157:9000';
-  //     case 'sejong':
-  //       return 'https://sejong.net.solidwallet.io';
-  //     case 'custom':
-  //       return 'http://13.209.103.183:9000';
-  //     case 'prep':
-  //     case 'np':
-  //       return 'http://20.20.7.156:9000';
-  //     case 'qa':
-  //       return 'https://devorg.icon.foundation';
-  //     default:
-  //   }
-  // }
-
-  // return '/';
+    if (configJson && configJson.WALLET_API_URL) {
+      return configJson.WALLET_API_URL;
+    }
 }
 
 export async function getIsSoloVersion() {
-  const configFile = await getConfigJsonFile();
-  if (configFile && configFile.IS_SOLO_VERSION) {
-    return !!configFile.IS_SOLO_VERSION;
-  }
-
-  if (process.env.REACT_APP_ENV) {
-    switch (process.env.REACT_APP_ENV) {
-      case 'mainnet':
-      case 'testnet':
-      case 'testnet1':
-      case 'sejong':
-      case 'custom':
-      case 'prep':
-      case 'np':
-      case 'qa':
-        return false;
-      default:
+    if (configJson && configJson.IS_SOLO_VERSION) {
+      return !!configJson.IS_SOLO_VERSION;
     }
-  }
-
-  return false;
-}
-
-// async function getConfigJsonFile() {
-//   try {
-//     const response = await fetch('/config.json');
-//     const responseJson = await response.json();
-//     console.log(process.env, "config response")
-//     return responseJson;
-//   } catch (e) {
-//     console.error(e);
-//     return {};
-//   }
-// }
-
-async function getConfigJsonFile() {
-  try {
-    // console.log(configJson, "config json on util")
-    return configJson
-  } catch(e) {
-    console.error(e)
-    return {}
-  }
 }
