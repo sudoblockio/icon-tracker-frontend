0ad1275f7b7e56f2d2514601d5d78efcb6bfad13
FidelVe <43641727+FidelVe@users.noreply.github.com>
2023-03-30 08:41:59 -0400

    Added write contract tab, many files needed to be changed in this commit to make sure all the components (write tab, redux saga, contract literals, etc) are created and/or modified correctly

--- src/components/CommonPage/DetailPage.js
+++ src/components/CommonPage/DetailPage.js
@@ -1,111 +1,107 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import {
-    startsWith,
-    findTabIndex,
-    noSpaceLowerCase,
-    isHxAddress
-} from '../../utils/utils'
-import {
-    NotFoundPage,
-    PendingPage
-} from '../../components';
+  startsWith,
+  findTabIndex,
+  noSpaceLowerCase,
+  isHxAddress
+} from "../../utils/utils";
+import { NotFoundPage, PendingPage } from "../../components";
 
 class DetailPage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      on: 0
+    };
+  }
 
-    constructor(props) {
-        super(props)
-        this.state = {
-            on: 0
-        }
-    }
+  componentDidMount() {
+    this.setInitialData(this.props.url);
+  }
 
-    componentDidMount() {
-        this.setInitialData(this.props.url)
+  componentWillReceiveProps(nextProps) {
+    const { pathname: currentPath } = this.props.url;
+    const { pathname: nextPath } = nextProps.url;
+    const { ROUTE } = this.props;
+    if (currentPath !== nextPath && startsWith(nextPath, ROUTE)) {
+      this.setInitialData(nextProps.url);
+      return;
+    } else {
+      const { hash: currentHash } = this.props.url;
+      const { hash: nextHash } = nextProps.url;
+      const { TABS: currentTabs } = this.props;
+      const { TABS: nextTabs } = nextProps;
+      if (currentHash !== nextHash || currentTabs.length !== nextTabs.length) {
+        this.setTab(findTabIndex(nextTabs, nextHash));
+      }
     }
+  }
 
-    componentWillReceiveProps(nextProps) {
-        const { pathname: currentPath } = this.props.url
-        const { pathname: nextPath } = nextProps.url
-        const { ROUTE } = this.props
-        if (currentPath !== nextPath && startsWith(nextPath, ROUTE)) {
-            this.setInitialData(nextProps.url)
-            return
-        }
-        else {
-            const { hash: currentHash } = this.props.url
-            const { hash: nextHash } = nextProps.url
-            const { TABS: currentTabs } = this.props
-            const { TABS: nextTabs } = nextProps
-            if (currentHash !== nextHash || currentTabs.length !== nextTabs.length) {
-                this.setTab(findTabIndex(nextTabs, nextHash))
-            }    
-        }
+  setInitialData = url => {
+    const query = url.pathname.split("/")[2];
+    if (query) {
+      const { TABS } = this.props;
+      this.props.getInfo(query);
+      this.setTab(findTabIndex(TABS, url.hash), query);
+      if (this.props.ROUTE === "/block") {
+        // Execute initial time for Internal TX
+        this.props.getList[1](query);
+      }
+      if (this.props.ROUTE === "/transaction") {
+        this.props.tokenTxList &&
+          this.props.tokenTxList({ transaction_hash: query });
+      }
     }
+  };
 
-    setInitialData = (url) => {
-        const query = url.pathname.split("/")[2]
-        if (query) {
-            const { TABS } = this.props
-            this.props.getInfo(query)
-            this.setTab(findTabIndex(TABS, url.hash), query)
-            if(this.props.ROUTE === "/block") {
-                // Execute initial time for Internal TX
-                this.props.getList[1](query);
-            }
-            if(this.props.ROUTE === "/transaction") {
-                this.props.tokenTxList && this.props.tokenTxList({transaction_hash: query})
-            }
-        }
-    }
+  setTab = (index, query) => {
+    const _index = index !== -1 ? index : 0;
+    this.setState({ on: _index }, () => {
+      this.setList(this.props.getList[_index], query);
+    });
+  };
 
-    setTab = (index, query) => {
-        const _index = index !== -1 ? index : 0
-        this.setState({ on: _index },
-            () => {
-                this.setList(this.props.getList[_index], query)                                
-            }
-        )
+  setList = (getListFunc, query) => {
+    const _query = query ? query : this.props.url.pathname.split("/")[2];
+    if (typeof getListFunc === "function") {
+      getListFunc(_query);
     }
+  };
 
-    setList = (getListFunc, query) => {
-        const _query = query ? query : this.props.url.pathname.split("/")[2]
-        if (typeof getListFunc === 'function') {
-            getListFunc(_query)
-        }
-    }
+  changeTab = index => {
+    const { TABS, url } = this.props;
+    const { pathname } = url;
+    this.props.history.push(`${pathname}#${noSpaceLowerCase(TABS[index])}`);
+  };
 
-    changeTab = (index) => {
-        const { TABS, url } = this.props
-        const { pathname } = url
-        this.props.history.push(`${pathname}#${noSpaceLowerCase(TABS[index])}`);
-    }
-    
-    render() {
-        const { loading, error, pending } = this.props;
-        const isNotFoundPage = !loading && error !== "" && !isHxAddress(error) && !pending
-        
-        const Content = () => {
-            if(pending){
-                return <PendingPage error={error}/>
-            } 
-            else if (isNotFoundPage) {
-                return <NotFoundPage error={error}/>
-            }
-            else {
-                const { InfoComponent, TabsComponent } = this.props
-                console.log(this.props, "the props detail page")
+  render() {
+    const { loading, error, pending } = this.props;
+    const isNotFoundPage =
+      !loading && error !== "" && !isHxAddress(error) && !pending;
 
-                return (
-                    <div className="content-wrap">
-                        
-                        <InfoComponent {...this.props}/>
-                        <TabsComponent {...this.props} {...this.state} changeTab={this.changeTab}/>
-                    </div>
-                )
-            }
-        }
-        return Content();
-    }
+    const Content = () => {
+      if (pending) {
+        return <PendingPage error={error} />;
+      } else if (isNotFoundPage) {
+        return <NotFoundPage error={error} />;
+      } else {
+        const { InfoComponent, TabsComponent } = this.props;
+        console.log(this.props, "the props detail page");
+
+        return (
+          <div className="content-wrap">
+            <InfoComponent {...this.props} />
+            <TabsComponent
+              {...this.props}
+              {...this.state}
+              changeTab={this.changeTab}
+            />
+          </div>
+        );
+      }
+    };
+    return Content();
+  }
 }
 
 export default DetailPage;
--- src/components/ContractsPage/ContractDetailPage/ContractTabs/index.js
+++ src/components/ContractsPage/ContractDetailPage/ContractTabs/index.js
@@ -1,97 +1,119 @@
-import React, { Component } from 'react';
-import { withRouter } from 'react-router-dom';
-import ContractTransactions from './ContractTransactions'
-import ContractInternalTransactions from './ContractInternalTransactions'
-import ContractTokenTransfers from './ContractTokenTransfers'
-import ContractCode from './ContractCode'
-import ContractRead from './ContractRead'
-import ContractEvents from './ContractEvents'
-import {
-    NoBox,
-    TabTable
-} from '../../../../components'
-import {
-    TX_TYPE,
-    CONTRACT_TABS,
-} from '../../../../utils/const'
-
+import React, { Component } from "react";
+import { withRouter } from "react-router-dom";
+import ContractTransactions from "./ContractTransactions";
+import ContractInternalTransactions from "./ContractInternalTransactions";
+import ContractTokenTransfers from "./ContractTokenTransfers";
+import ContractCode from "./ContractCode";
+import ContractRead from "./ContractRead";
+import ContractEvents from "./ContractEvents";
+import ContractWrite from "./ContractWrite";
+import { NoBox, TabTable } from "../../../../components";
+import { TX_TYPE, CONTRACT_TABS } from "../../../../utils/const";
 
 class ContractTabs extends Component {
-
-    render() {
-        const { on, contract, contractTx, contractInternalTx, contractTokenTx, contractEvents, contractAbi, contractReadInfo } = this.props
-        console.log(this.props, "larger comp props")
-        const { loading, data } = contract
-        console.log(contract, "the contract")
-        const { address } = data
-        return (
-            <TabTable
-                {...this.props}
-                onClickTab = {this.props.changeTab}
-                TABS={CONTRACT_TABS}
-                on={on}
-                loading={loading}
-                TableContents={on => {
-                    switch (on) {
-                        case 0:
-                            return (
-                                <ContractTransactions 
-                                    txData={contractTx}
-                                    goAllTx={() => {this.props.history.push(`/${TX_TYPE.CONTRACT_TX}/${address}`)}} 
-                                    txType={TX_TYPE.CONTRACT_TX}
-                                    address={address}
-                                />
-                            )
-                        case 1:
-                            return (
-                                <ContractInternalTransactions 
-                                    txData={contractInternalTx}
-                                    goAllTx={() => {this.props.history.push(`/${TX_TYPE.CONTRACT_INTERNAL_TX}/${address}`)}} 
-                                    txType={TX_TYPE.CONTRACT_INTERNAL_TX}
-                                    address={address}
-                                />
-                            )
-                        case 2:
-                            return (
-                                <ContractTokenTransfers 
-                                    txData={contractTokenTx}
-                                    goAllTx={() => {this.props.history.push(`/${TX_TYPE.CONTRACT_TOKEN_TX}/${address}`)}} 
-                                    txType={TX_TYPE.CONTRACT_TOKEN_TX}
-                                    address={address}
-                                />
-                            )
-                        case 3:
-                            return (
-                                <ContractCode 
-                                    contract={contract} 
-                                    contractAbi={contractAbi}
-                                />
-                            )
-                        case 4:
-                            return (
-                                <ContractRead
-                                    contract={contract}                            
-                                    contractReadInfo={contractReadInfo}
-                                    icxCall={this.props.icxCall}
-                                />
-                            )
-                        case 5:
-                            return (
-                                <ContractEvents
-                                    txData={contractEvents}                            
-                                    goAllTx={() => {this.props.history.push(`/${TX_TYPE.CONTRACT_EVENTS}/${address}`)}} 
-                                    txType={TX_TYPE.CONTRACT_EVENTS}
-                                />
-                            )
-                        default:
-                            return <NoBox text="No Data" />
-                    }}
-                }
-            />
-        )
-    }
+  render() {
+    const {
+      on,
+      contract,
+      contractTx,
+      contractInternalTx,
+      contractTokenTx,
+      contractEvents,
+      contractAbi,
+      contractReadInfo,
+      contractWriteInfo
+    } = this.props;
+    console.log(this.props, "larger comp props");
+    const { loading, data } = contract;
+    console.log(contract, "the contract");
+    const { address } = data;
+    return (
+      <TabTable
+        {...this.props}
+        onClickTab={this.props.changeTab}
+        TABS={CONTRACT_TABS}
+        on={on}
+        loading={loading}
+        TableContents={on => {
+          switch (on) {
+            case 0:
+              return (
+                <ContractTransactions
+                  txData={contractTx}
+                  goAllTx={() => {
+                    this.props.history.push(
+                      `/${TX_TYPE.CONTRACT_TX}/${address}`
+                    );
+                  }}
+                  txType={TX_TYPE.CONTRACT_TX}
+                  address={address}
+                />
+              );
+            case 1:
+              return (
+                <ContractInternalTransactions
+                  txData={contractInternalTx}
+                  goAllTx={() => {
+                    this.props.history.push(
+                      `/${TX_TYPE.CONTRACT_INTERNAL_TX}/${address}`
+                    );
+                  }}
+                  txType={TX_TYPE.CONTRACT_INTERNAL_TX}
+                  address={address}
+                />
+              );
+            case 2:
+              return (
+                <ContractTokenTransfers
+                  txData={contractTokenTx}
+                  goAllTx={() => {
+                    this.props.history.push(
+                      `/${TX_TYPE.CONTRACT_TOKEN_TX}/${address}`
+                    );
+                  }}
+                  txType={TX_TYPE.CONTRACT_TOKEN_TX}
+                  address={address}
+                />
+              );
+            case 3:
+              return (
+                <ContractCode contract={contract} contractAbi={contractAbi} />
+              );
+            case 4:
+              return (
+                <ContractRead
+                  contract={contract}
+                  contractReadInfo={contractReadInfo}
+                  icxCall={this.props.icxCall}
+                />
+              );
+            case 5:
+              return (
+                <ContractWrite
+                  contract={contract}
+                  contractWriteInfo={contractWriteInfo}
+                  icxCall={this.props.icxCall}
+                />
+              );
+            case 6:
+              return (
+                <ContractEvents
+                  txData={contractEvents}
+                  goAllTx={() => {
+                    this.props.history.push(
+                      `/${TX_TYPE.CONTRACT_EVENTS}/${address}`
+                    );
+                  }}
+                  txType={TX_TYPE.CONTRACT_EVENTS}
+                />
+              );
+            default:
+              return <NoBox text="No Data" />;
+          }
+        }}
+      />
+    );
+  }
 }
 
 export default withRouter(ContractTabs);
-
-
