10353dd3c0ad2101bc66eb10f86cd995f6191a11
FidelVe <43641727+FidelVe@users.noreply.github.com>
2023-04-10 10:09:18 -0400

    squash merge from local dev branch to update pull request. chores done: refactoring contract tab

--- src/components/ContractsPage/ContractDetailPage/ContractTabs/MiscContractComponents.js
+++ src/components/ContractsPage/ContractDetailPage/ContractTabs/MiscContractComponents.js
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import BigNumber from "bignumber.js";
+import styles from "./MiscContractComponents.module.css";
 
 function ReadMethodItems({
   methods,
@@ -134,13 +135,7 @@ function Inputs({ methodName, inputs, params, handleChange }) {
   });
 }
 
-function InputItem({
-  methodName,
-  params,
-  handleChange,
-  item,
-  index
-}) {
+function InputItem({ methodName, params, handleChange, item, index }) {
   const name = item["name"];
   const type = item["type"];
   const inputName = `${methodName}_${name}_${type}`;
@@ -317,9 +312,316 @@ function WriteMethodItem({
     <Outputs key={index} func={func} outputs={outputs} index={index} />
   );
 }
+// function ContractWrite({ contract, contractWriteInfo }) {
+//   console.log('contractWriteInfo');
+//   console.log(contractWriteInfo);
+//   const [paramsState, setParamsState] = useState({});
+//   const { data } = contract;
+//   const { address: address } = data;
+//   const loading = contractWriteInfo != null ? contractWriteInfo.loading : true;
+
+//   return (
+//     <div className="contents">
+//       <div className="code-box read">
+//         <div className="title-group">
+//           <span className="title">Write contract information</span>
+//         </div>
+//         {loading ? (
+//           <LoadingComponent height="322px" />
+//         ) : (
+//           <div className="scroll">
+//             <div className="list">
+//               {!!contractWriteInfo.error ? (
+//                 <div>{contractWriteInfo.error}</div>
+//               ) : (
+//                 <ListOfWriteMethods
+//                 arrayOfMethods={contractWriteInfo.funcList}
+//                 />
+//               )}
+//             </div>
+//           </div>
+//         )}
+//       </div>
+//     </div>
+//   );
+// }
+
+// function ListOfWriteMethods({ arrayOfMethods }) {
+//   const [paramsState, setParamsState] = useState({});
+//   function handleChange(e) {
+//     const { name, value } = e.target;
+//     setParamsState(state => {
+//       const newState = { ...state, [name]: value };
+//       return newState;
+//     });
+//   }
+
+//   return (
+//     <div className={styles.writeMethodsMain}>
+//       {arrayOfMethods.map((writeMethod, index) => {
+//         return (
+//           <div
+//             key={`writeMethods-${index}`}
+//             style={{ width: "100%" }}
+//           >
+//           <CollapsibleMethodItem writeMethod={writeMethod} index={index} />
+//           </div>
+//         );
+//       })}
+//     </div>
+//   );
+// }
+
+function CollapsibleMethodItem({
+  methodInput,
+  methodName,
+  methodOutput,
+  index,
+  params,
+  handleChangeParent,
+  handleClick,
+  address
+}) {
+  // const [valueState, setValueState] = useState("");
+  const [isOpen, setIsOpen] = useState(false);
+  const outputType =
+    methodInput.readonly != null && methodInput.readonly === "0x1"
+      ? methodInput.outputs[0].type
+      : "";
+  const parsedMethodOutput =
+    methodInput.readonly != null && methodInput.readonly === "0x1"
+      ? JSON.stringify(methodOutput.valueArray[0])
+      : "";
+  const isExpandable =
+    methodInput.readonly != null && methodInput.readonly === "0x1"
+      ? methodInput.inputs.length > 0
+        ? true
+        : methodInput.outputs[0].type === "dict" ||
+          methodInput.outputs[0].type === "list"
+        ? true
+        : false
+      : true;
+
+  // function handleChange(e) {
+  //   const { value } = e.target;
+  //   setValueState(value);
+  // }
+
+  function toggleOpen() {
+    setIsOpen(state => !state);
+  }
+
+  function handleButtonClick() {
+    handleClick(address, methodName, methodInput.inputs, index);
+  }
+
+  console.log("method name");
+  console.log(methodName);
+  console.log(methodInput);
+  console.log(methodOutput);
+  return (
+    <div
+      className={
+        !isExpandable
+          ? `${styles.writeMethodContainer} ${styles.writeMethodContainerClosed}`
+          : isOpen
+          ? `${styles.writeMethodContainer} ${styles.writeMethodContainerOpen}`
+          : `${styles.writeMethodContainer} ${styles.writeMethodContainerClosed}`
+      }
+    >
+      <div
+        className={
+          isExpandable
+            ? `${styles.writeMethodTitle} ${styles.writeMethodTitleExpandable}`
+            : `${styles.writeMethodTitle}`
+        }
+        onClick={toggleOpen}
+      >
+        <div className={styles.writeMethodTitleLeft}>
+          <span>{index + 1}.</span>
+          <span>{methodName}</span>{" "}
+          {!isExpandable && (
+            <span className={styles.writeMethodTitleLeftOutput}>
+              {parsedMethodOutput}
+            </span>
+          )}
+          <span>
+            <em>{outputType}</em>
+          </span>{" "}
+        </div>
+        {isExpandable && (
+          <div className={styles.writeMethodTitleRight}>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className={
+                !isOpen
+                  ? `${styles.writeMethodTitleIcon}`
+                  : `${styles.writeMethodTitleIcon} ${styles.writeMethodTitleIconRotated}`
+              }
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M19.5 8.25l-7.5 7.5-7.5-7.5"
+              />
+            </svg>
+          </div>
+        )}
+      </div>
+      {methodInput.inputs.length > 0 && (
+        <div className={styles.writeMethodBody}>
+          {methodInput.inputs.map((input, index2) => {
+            const name = input["name"];
+            const type = input["type"];
+            const inputName = `${methodName}_${name}_${type}`;
+            const placeholder = `${name} (${type})`;
+            const value = params[inputName] || "";
+
+            return (
+              <div
+                className={styles.writeMethodBodyInput}
+                key={`writeMethod-element-${index2}`}
+              >
+                <div className={styles.writeMethodBodyInputName}>
+                  {methodName}
+                </div>
+                <div className={styles.writeMethodBodyInputType}>
+                  <input
+                    type="text"
+                    className="over"
+                    key={`writeMethod-${index2}`}
+                    name={inputName}
+                    placeholder={placeholder}
+                    value={value}
+                    onChange={handleChangeParent}
+                  />
+                </div>
+              </div>
+            );
+          })}
+          <div className={styles.methodInputButtonContainer}>
+            <button
+              className={styles.methodInputButton}
+              onClick={handleButtonClick}
+            >
+              Query
+            </button>
+          </div>
+        </div>
+      )}
+      <div
+        className={
+          methodOutput.error === ""
+            ? `${styles.writeMethodBodyOutput} ${styles.writeMethodBodyOutputSuccess}`
+            : `${styles.writeMethodOutput} ${styles.writeMethodOutputError}`
+        }
+      >
+        <p>Response:</p>
+        <p>{JSON.stringify(methodOutput.valueArray[0])}</p>
+      </div>
+    </div>
+  );
+}
+
+function ReadMethodItems2({
+  methods,
+  params,
+  handleChange,
+  handleClick,
+  address
+}) {
+  return (
+    <ul className="list">
+      {methods.readOnlyMethodsNameArray.map((methodName, index) => {
+        return (
+          <div key={`MethodItem-${methodName}-${index}`}>
+            <CollapsibleMethodItem
+              methodInput={methods[methodName].inputs}
+              methodName={methodName}
+              methodOutput={methods[methodName].outputs}
+              index={index}
+              params={params}
+              handleChangeParent={handleChange}
+              handleClick={handleClick}
+              address={address}
+            />
+          </div>
+        );
+      })}
+    </ul>
+  );
+}
+
+function ReadMethodItem2({
+  methodName,
+  methodData,
+  index,
+  params,
+  handleChange,
+  handleClick,
+  address
+}) {
+  const outputs = methodData.outputs;
+  const inputs = methodData.inputs;
+  const isQuery = inputs.inputs.length > 0;
+
+  function handleButtonClick() {
+    handleClick(address, methodName, inputs.inputs, index);
+  }
+
+  return isQuery ? (
+    <>
+      <li key="li0" className="input">
+        <span className="label">
+          {index + 1}. {methodName} >{" "}
+        </span>
+        <Inputs
+          methodName={methodName}
+          inputs={inputs.inputs}
+          params={params}
+          handleChange={handleChange}
+          methodName={methodName}
+        />
+        <button
+          key="button"
+          className="btn-type-query"
+          onClick={handleButtonClick}
+        >
+          Query
+        </button>
+      </li>
+      ,
+      <li key="li1" className="result">
+        <OutputTypes func={inputs} />
+        {!isEmptyOutput(outputs) && (
+          <OutputResults
+            methodName={methodName}
+            func={inputs}
+            outputs={outputs}
+          />
+        )}
+      </li>
+    </>
+  ) : (
+    <Outputs
+      key={index}
+      methodName={methodName}
+      func={inputs}
+      outputs={outputs}
+      index={index}
+    />
+  );
+}
 
 const MiscComponents = {
   ReadMethodItems,
-  WriteMethodItems
+  ReadMethodItems2,
+  WriteMethodItems,
+  CollapsibleMethodItem
 };
+
 export default MiscComponents;
