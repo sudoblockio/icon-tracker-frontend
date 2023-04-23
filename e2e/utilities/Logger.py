e7bac3afe79e862bdf972c2373f5e2431478ce63
robcxyz <rob@sudoblock.io>
2023-04-23 22:10:08 +0700

    chore: add absolute paths to e2e scripts so they can be run anywhere

--- e2e/utilities/Logger.py
+++ e2e/utilities/Logger.py
@@ -1,11 +1,17 @@
 import logging
+import os
 
 
 class LogGen:
     @staticmethod
     def loggen():
-        logging.basicConfig(filename="./e2e/logs/automation.log",
-                            format='%(asctime)s: %(levelname)s: %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p', force=True)
+        log_file = os.path.join(
+            os.path.dirname(__file__), "../../e2e/logs/automation.log"
+        )
+        logging.basicConfig(
+            filename=log_file,
+            format='%(asctime)s: %(levelname)s: %(message)s',
+            datefmt='%m/%d/%Y %I:%M:%S %p', force=True)
         logger = logging.getLogger()
         logger.setLevel(logging.INFO)
         return logger
