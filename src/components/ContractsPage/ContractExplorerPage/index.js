c9f4c5f5a930b3cb039e3da7c953fbf3a99b4d00
FidelVe <43641727+FidelVe@users.noreply.github.com>
2023-04-28 10:30:01 -0400

    building single page

--- src/components/ContractsPage/ContractExplorerPage/index.js
+++ src/components/ContractsPage/ContractExplorerPage/index.js
@@ -1,16 +1,69 @@
-import React, { Component } from 'react';
+import React, { useState } from "react";
+import MiscComponents from "./MiscContractComponents";
+import ButtonSet from "./ButtonSet";
 import styles from "./ContractExplorerPage.module.css";
 
-class ContractExplorerPage extends Component {
-	constructor(props) {
-		super(props);
-	}
-	
-	render() {
+const { ReadMethod, WriteMethod } = MiscComponents;
 
+function ContractExplorerPage(props) {
+  const [activeSection, setActiveSection] = useState(0);
 
-		{/* return <DetailPage {...this.props} /> */}
-		return <div className={styles.main} >ContractExplorerPage</div>
-	}
+  return (
+    <div className={styles.main}>
+      <div className={styles.mainContainer}>
+        <div className={styles.pageTitle}>Contract Writer</div>
+        <div className={`${styles.pageContent}`}>
+          <div
+            className={`${styles.pageContentHeader} ${styles.paperContainer}`}
+          >
+            <InputItem  label={`Address`}/>
+            <Separator />
+            <DropdownItem label={`Network`} />
+          </div>
+          <div className={`${styles.pageContentBody} ${styles.paperContainer}`}>
+            <ButtonSet 
+              activeButton={activeSection}
+              handleActiveChange={setActiveSection}
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function InputItem({ label }) {
+  return (
+    <div className={styles.inputItem}>
+      <div className={styles.inputItemLabel}>{label}</div>
+      <div className={styles.inputItemContent}>
+        <input
+          className={styles.inputItemInput}
+          type="text"
+          placeholder="cx0000.."
+        />
+      </div>
+    </div>
+  )
+}
+
+function DropdownItem({ label }) {
+  return (
+    <div className={styles.dropdownItem}>
+      <div className={styles.dropdownItemLabel}>{label}</div>
+      <div className={styles.dropdownItemContent}>
+        <select className={styles.dropdownItemSelect}>
+          <option value="mainnet">Mainnet</option>
+          <option value="berlin">Berlin</option>
+          <option value="lisbon">Lisbon</option>
+          <option value="custom">Custom</option>
+        </select>
+      </div>
+    </div>
+  )
+}
+
+function Separator() {
+  return <div className={styles.separator}></div>;
 }
 export default ContractExplorerPage;
