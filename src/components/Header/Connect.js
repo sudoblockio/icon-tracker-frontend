928c6458d1c92c2dfa193dd75f34cd36952fb08d
FidelVe <43641727+FidelVe@users.noreply.github.com>
2023-08-03 00:02:08 -0400

    Chore: refactored Connect.js component into a react function component

--- src/components/Header/Connect.js
+++ src/components/Header/Connect.js
@@ -1,103 +1,86 @@
-import React, { Component } from 'react'
-import { requestAddress } from '../../utils/connect'
-import { CopyButton } from '../../components'
-import checkIconex from 'check-iconex'
-import NotificationManager from '../../utils/NotificationManager'
+import React, { useState, useEffect } from 'react';
+import { requestAddress } from '../../utils/connect';
+import { CopyButton } from '../../components';
+import checkIconex from 'check-iconex';
+import NotificationManager from '../../utils/NotificationManager';
 
-class Connect extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            disabled: false,
-            walletAddress: this.props.walletAddress,
-        }
-    }
+function Connect(props) {
+    const [disabled, setDisabled] = useState(false);
+    const [walletAddress, setWalletAddress] = useState(props.walletAddress);
 
-    async componentDidMount() {
-        const { isChrome, iconexInstalled, hasIconWallet } = await checkIconex(1000, 2000)
-        this.setState({
-            disabled: !(isChrome && iconexInstalled && hasIconWallet),
-        })
-    }
+    useEffect(() => {
+        const fetchIconexStatus = async () => {
+            const { isChrome, iconexInstalled, hasIconWallet } = await checkIconex(1000, 2000);
+            setDisabled(!(isChrome && iconexInstalled && hasIconWallet));
+        };
+        fetchIconexStatus();
+    }, []);
 
-    componentWillReceiveProps(nextProps) {
-        if (nextProps.walletAddress !== this.props.walletAddress) {
-            this.setState(
-                {
-                    walletAddress: nextProps.walletAddress,
-                },
-                () => {
-                    window.dispatchEvent(
-                        new CustomEvent('CUSTOM_FX', {
-                            detail: { type: 'SET_WALLET' },
-                        }),
-                    )
-                },
-            )
-        }
-    }
-
-    getWalletAddress = async () => {
-        if (this.state.disabled) {
-            window.open('https://chrome.google.com/webstore/detail/iconex/flpiciilemghbmfalicajoolhkkenfel', '_blank')
-            return
-        }
-
-        if (this.state.walletAddress) {
-            return
-        }
-        
-        const walletAddress = await requestAddress()
-        this.setState({ walletAddress }, () => {
+    useEffect(() => {
+        if (props.walletAddress !== walletAddress) {
+            setWalletAddress(props.walletAddress);
             window.dispatchEvent(
                 new CustomEvent('CUSTOM_FX', {
                     detail: { type: 'SET_WALLET' },
-                }),
-            )
-            this.props.setAddress(walletAddress)
-            this.props.history.push(`/address/${walletAddress}`)
-        })
-    }
+                })
+            );
+        }
+    }, [props.walletAddress]);
 
+    const getWalletAddress = async () => {
+        if (disabled) {
+            window.open('https://chrome.google.com/webstore/detail/iconex/flpiciilemghbmfalicajoolhkkenfel', '_blank');
+            return;
+        }
 
-    disconnect = () => {
-        this.setState({ walletAddress: undefined }, () => {
-            this.props.clearWallet()
-            NotificationManager.deregisterServiceWorker()
-        })
-    }
+        if (walletAddress) {
+            return;
+        }
 
-    render() {
-        const { walletAddress } = this.state
-        return (
-            <div className={`connect ${walletAddress ? 'join' : ''}`}>
-                <span onClick={this.getWalletAddress}>
-                    <em className="img" />
-                </span>
-                {walletAddress ? (
-                    <div className="sub-menu">
-                        <p>
-                            <span>Wallet Address</span>
-                            <CopyButton data={walletAddress} title={'Copy Address'} wallet={true} />
-                        </p>
-                        <span className="btn" onClick={this.disconnect}>
-                            Disconnect
-                        </span>
-                        <span
-                            className="btn"
-                            onClick={() => {
-                                this.props.history.push(`/address/${walletAddress}`)
-                            }}
-                        >
-                            View Details
-                        </span>
-                    </div>
-                ) : (
-                    ''
-                )}
-            </div>
-        )
-    }
+        const address = await requestAddress();
+        setWalletAddress(address);
+        window.dispatchEvent(
+            new CustomEvent('CUSTOM_FX', {
+                detail: { type: 'SET_WALLET' },
+            })
+        );
+        props.setAddress(address);
+        props.history.push(`/address/${address}`);
+    };
+
+    const disconnect = () => {
+        setWalletAddress(undefined);
+        props.clearWallet();
+        NotificationManager.deregisterServiceWorker();
+    };
+
+    return (
+        <div className={`connect ${walletAddress ? 'join' : ''}`}>
+            <span onClick={getWalletAddress}>
+                <em className="img" />
+            </span>
+            {walletAddress ? (
+                <div className="sub-menu">
+                    <p>
+                        <span>Wallet Address</span>
+                        <CopyButton data={walletAddress} title={'Copy Address'} wallet={true} />
+                    </p>
+                    <span className="btn" onClick={disconnect}>
+                        Disconnect
+                    </span>
+                    <span
+                        className="btn"
+                        onClick={() => {
+                            props.history.push(`/address/${walletAddress}`);
+                        }}
+                    >
+                        View Details
+                    </span>
+                </div>
+            ) : null}
+        </div>
+    );
 }
 
-export default Connect
+export default Connect;
+
