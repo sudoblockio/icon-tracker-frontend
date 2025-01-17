34318451aa55ec11a8259983a8a138d022055f2b
atif <atif_hossain@live.com>
2025-01-17 17:03:41 +0530

    fix: sentry environment

--- src/index.js
+++ src/index.js
@@ -20,19 +20,21 @@ Sentry.init({
         Sentry.browserProfilingIntegration(),
         Sentry.replayIntegration(),
     ],
-    // Tracing
+    environment: config.network,
     tracesSampleRate: 1.0, //  Capture 100% of the transactions
-    // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
     tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+    // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+    profilesSampleRate: 1.0,
     // Set profilesSampleRate to 1.0 to profile every transaction.
     // Since profilesSampleRate is relative to tracesSampleRate,
     // the final profiling rate can be computed as tracesSampleRate * profilesSampleRate
     // For example, a tracesSampleRate of 0.5 and profilesSampleRate of 0.5 would
     // results in 25% of transactions being profiled (0.5*0.5=0.25)
-    profilesSampleRate: 1.0,
     // Session Replay
-    replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
-    replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+    replaysSessionSampleRate: 0.1,
+    // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+    replaysOnErrorSampleRate: 1.0,
+    // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
 });
 
 ReactDOM.render(
